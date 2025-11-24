ecaab678cc6673723dbe997dafef810aa0656280
StefanNeuberger <stef.neuberger@gmail.com>
2025-11-24 14:46:06 +0100

    Update TestcontainersConfiguration.java

--- backend/src/test/java/com/team18/backend/TestcontainersConfiguration.java
+++ backend/src/test/java/com/team18/backend/TestcontainersConfiguration.java
@@ -7,12 +7,12 @@ import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
+public class TestcontainersConfiguration {
 
     @Bean
     @ServiceConnection
     MongoDBContainer mongoDbContainer() {
-        return new MongoDBContainer( DockerImageName.parse( "mongo:latest" ) );
+        return new MongoDBContainer( DockerImageName.parse( "mongo:7.0.14" ) );
     }
 
 }
