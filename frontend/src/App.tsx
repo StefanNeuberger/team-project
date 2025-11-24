3a072a398e8c8444d6759451cc8b20159a7855a4
Simon Sagstetter <simon.sagstetter95@gmail.com>
2025-11-24 14:07:42 +0100

    React Router DOM Setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
+import {RouterProvider} from "react-router/dom";
+import {router} from "./routes/default";
+
 function App() {
   return (
-    <>
-
-    </>
+    <RouterProvider router={router}/>
   )
 }
 
