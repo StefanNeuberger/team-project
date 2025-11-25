177823384f50f976531b6ce1654076fd34f500dd
StefanNeuberger <stef.neuberger@gmail.com>
2025-11-25 20:12:07 +0100

    Create orval.config.ts

---
+++ frontend/orval.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "orval";
+
+export default defineConfig({
+  shopApi: {
+    input: {
+      target: "./openapi.json",
+    },
+    output: {
+      mode: "tags-split",
+      target: "src/api/generated",
+      client: "react-query",
+      baseUrl: "http://localhost:8080",
+      httpClient: "fetch",
+    },
+  },
+});
