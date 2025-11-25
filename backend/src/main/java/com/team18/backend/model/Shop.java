427f69fdcb877e192455379c435a697ae2946783
StefanNeuberger <stef.neuberger@gmail.com>
2025-11-25 20:33:18 +0100

    add model validation annotations

--- backend/src/main/java/com/team18/backend/model/Shop.java
+++ backend/src/main/java/com/team18/backend/model/Shop.java
@@ -1,11 +1,15 @@
 package com.team18.backend.model;
 
 import org.springframework.data.mongodb.core.mapping.Document;
+import io.swagger.v3.oas.annotations.media.Schema;
+
 
 
 @Document(collection = "shops")
+@Schema(description = "Shop entity")
 public class Shop extends BaseModel {
 
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED, nullable = false)
     private String name;
 
     public Shop() {
