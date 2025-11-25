1df6c33fa4e1302e1051f6d57b4f17665afac405
Jan Schwarz <jan-paul@schw-a-rz.de>
2025-11-25 17:40:33 +0100

    added `ErrorResponse` record and overridden `toString`, `equals`, and `hashCode` methods in `ItemDTO`

--- backend/src/main/java/com/team18/backend/dto/ItemDTO.java
+++ backend/src/main/java/com/team18/backend/dto/ItemDTO.java
@@ -2,6 +2,8 @@ package com.team18.backend.dto;
 
 import com.team18.backend.model.BaseModel;
 
+import java.util.Objects;
+
 public class ItemDTO extends BaseModel {
     private String sku;
     private String name;
@@ -28,4 +30,23 @@ public class ItemDTO extends BaseModel {
         this.name = name;
     }
 
+    @Override
+    public String toString() {
+        return "ItemDTO{" +
+                "sku='" + sku + '\'' +
+                ", name='" + name + '\'' +
+                "} " + super.toString();
+    }
+
+    @Override
+    public boolean equals( Object o ) {
+        if ( o == null || getClass() != o.getClass() ) return false;
+        ItemDTO itemDTO = ( ItemDTO ) o;
+        return Objects.equals( sku, itemDTO.sku ) && Objects.equals( name, itemDTO.name );
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash( sku, name );
+    }
 }
