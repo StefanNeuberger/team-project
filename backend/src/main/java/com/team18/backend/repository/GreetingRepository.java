01bb879d12810fb77409ab20d9e25c4adcf2c33c
StefanNeuberger <stef.neuberger@gmail.com>
2025-11-25 17:23:11 +0100

    DONT GREET

--- backend/src/main/java/com/team18/backend/repository/GreetingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.team18.backend.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.team18.backend.model.Greeting;
-
-public interface GreetingRepository extends MongoRepository<Greeting, String> {
-}
-
