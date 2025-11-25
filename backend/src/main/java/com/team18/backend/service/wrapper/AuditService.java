8bef95eb60824f79994b338c760eeab9218a9e3f
Simon Sagstetter <simon.sagstetter95@gmail.com>
2025-11-25 13:46:42 +0100

    Remove constructor from AuditService Wrapper

--- backend/src/main/java/com/team18/backend/service/wrapper/AuditService.java
+++ backend/src/main/java/com/team18/backend/service/wrapper/AuditService.java
@@ -6,9 +6,6 @@ import java.time.Instant;
 
 @Component
 public class AuditService {
-
-    public AuditService() {}
-
     public Instant getCurrentTimestamp() {
         return Instant.now();
     }
