c78523381f0f68d77616e2f63af722ac95660555
StefanNeuberger <stef.neuberger@gmail.com>
2025-11-26 15:03:24 +0100

    Update ShipmentRepository.java

--- backend/src/main/java/com/team18/backend/repository/ShipmentRepository.java
+++ backend/src/main/java/com/team18/backend/repository/ShipmentRepository.java
@@ -8,9 +8,9 @@ import java.util.List;
 
 public interface ShipmentRepository extends MongoRepository<Shipment, String> {
     
-    List<Shipment> findByWarehouseId(String warehouseId);
-    
+    List<Shipment> findByWarehouseId( String warehouseId );
+
     @Query("{'warehouseId': {'$in': ?0}}")
-    List<Shipment> findAllByWarehouseIdIn(List<String> warehouseIds);
+    List<Shipment> findAllByWarehouseIdIn( List<String> warehouseIds );
 }
 
