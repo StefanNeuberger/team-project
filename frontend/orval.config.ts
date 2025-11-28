7e0813c33f35afa13cf3a2414cd656f0fba67d76
StefanNeuberger <stef.neuberger@gmail.com>
2025-11-28 10:53:31 +0100

    dockerize (#58)
    * dockerize
    
    * CI deploy
    
    * Update deploy.yaml
    
    needs spring
    
    * Update deploy.yaml
    
    use mvn not the wrapper
    
    * Update deploy.yaml
    
    we need mongodb
    
    * Update Dockerfile
    
    * Update deploy.yaml
    
    * Update deploy.yaml
    
    * SpaController
    
    * Update SpaController.java
    
    * add favicon
    
    * remove quiet option from buildl script
    
    * Create SpaControllerTest.java
    
    * test index.html
    
    * Update SpaController.java

--- frontend/orval.config.ts
+++ frontend/orval.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from "orval";
 
+const isDev = process.env.NODE_ENV !== "production";
+
+const baseURL = isDev
+  ? "http://localhost:8080"
+  : "https://teamproject007-c48cf54d7f7d.herokuapp.com";
 export default defineConfig({
   shopApi: {
     input: {
@@ -9,7 +14,7 @@ export default defineConfig({
       mode: "tags-split",
       target: "src/api/generated",
       client: "react-query",
-      baseUrl: "http://localhost:8080",
+      baseUrl: baseURL,
     },
   },
 });
