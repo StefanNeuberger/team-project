4355af4d38321dd9960128e334257fed531c660d
StefanNeuberger <stef.neuberger@gmail.com>
2025-11-28 12:26:53 +0100

    shopsPage and basic shopPage

--- frontend/src/components/BaseNavigation.tsx
+++ frontend/src/components/BaseNavigation.tsx
@@ -1,55 +1,60 @@
 import {
-    NavigationMenu,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-} from "@/components/ui/navigation-menu"
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
 import { NavLink, useLocation, useParams } from "react-router-dom";
 
 type NavigationLinkType = {
-    name: string;
-    href: string;
-}
+  name: string;
+  href: string;
+};
 
+export default function BaseNavigation({
+  isMobile,
+  toggleMenu,
+}: Readonly<{
+  isMobile?: boolean;
+  toggleMenu?: () => void;
+}>) {
+  const { shopId } = useParams();
 
-export default function BaseNavigation( { isMobile, toggleMenu }: Readonly<{
-    isMobile?: boolean,
-    toggleMenu?: () => void
-}> ) {
+  const navigationLinks: NavigationLinkType[] = [
+    { name: "Home", href: `/shop` },
+    { name: "Items", href: `/shop/${shopId}/items` },
+    { name: "Warehouses", href: `/shop/${shopId}/warehouses` },
+    { name: "Inventory", href: `/shop/${shopId}/inventory` },
+    { name: "Shipments", href: `/shop/${shopId}/shipments` },
+  ];
 
-    const { shopId } = useParams();
+  const handleLinkClick = () => {
+    if (isMobile && toggleMenu) {
+      toggleMenu();
+    }
+  };
 
-    const navigationLinks: NavigationLinkType[] = [
-        { name: 'Items', href: `/shop/${ shopId }/items` },
-        { name: 'Warehouses', href: `/shop/${ shopId }/warehouses` },
-        { name: 'Inventory', href: `/shop/${ shopId }/inventory` },
-        { name: 'Shipments', href: `/shop/${ shopId }/shipments` },
-    ];
+  const { pathname } = useLocation();
 
-    const handleLinkClick = () => {
-        if ( isMobile && toggleMenu ) {
-            toggleMenu();
-        }
-    };
+  const orientation = isMobile ? "vertical" : "horizontal";
 
-    const { pathname } = useLocation();
-
-    const orientation = isMobile ? 'vertical' : 'horizontal';
-
-    return (
-        <NavigationMenu className={ "items-start" } orientation={ orientation }>
-            <NavigationMenuList className={ "flex-col items-start md:flex-row" }>
-                { navigationLinks.map( ( { name, href } ) => (
-                    <NavigationMenuItem key={ name }>
-                        <NavigationMenuLink data-active={ pathname === href }
-                                            className={ "data-[active=true]:text-accent-foreground" } asChild>
-                            <NavLink onClick={ handleLinkClick } to={ href }>
-                                { name }
-                            </NavLink>
-                        </NavigationMenuLink>
-                    </NavigationMenuItem>
-                ) ) }
-            </NavigationMenuList>
-        </NavigationMenu>
-    )
+  return (
+    <NavigationMenu className={"items-start"} orientation={orientation}>
+      <NavigationMenuList className={"flex-col items-start md:flex-row"}>
+        {navigationLinks.map(({ name, href }) => (
+          <NavigationMenuItem key={name}>
+            <NavigationMenuLink
+              data-active={pathname === href}
+              className={"data-[active=true]:text-accent-foreground"}
+              asChild
+            >
+              <NavLink onClick={handleLinkClick} to={href}>
+                {name}
+              </NavLink>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        ))}
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
 }
--- frontend/src/layouts/ShopLayout.tsx
+++ frontend/src/layouts/ShopLayout.tsx
@@ -1,16 +1,17 @@
 import Footer from "@/components/Footer.tsx";
 import { Outlet } from "react-router-dom";
 import Header from "@/components/Header.tsx";
+import { Toaster } from "sonner";
 
 export default function ShopLayout() {
-
-    return (
-        <>
-            <Header/>
-            <div className={ "flex-1 flex" }>
-                <Outlet/>
-            </div>
-            <Footer/>
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <div className="flex flex-col flex-1 items-center justify-center py-8">
+        <Outlet />
+      </div>
+      <Toaster />
+      <Footer />
+    </>
+  );
 }
