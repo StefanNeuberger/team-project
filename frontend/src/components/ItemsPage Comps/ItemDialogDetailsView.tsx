f99777e8e479159e3b52e8d28f8754018467dabb
Jan Schwarz <jan-paul@schw-a-rz.de>
2025-12-01 16:57:58 +0100

    Add delete confirmation to ItemDialogDetailsView and enhance item filtering in ItemsView

--- frontend/src/components/ItemsPage Comps/ItemDialogDetailsView.tsx
+++ frontend/src/components/ItemsPage Comps/ItemDialogDetailsView.tsx
@@ -1,25 +1,34 @@
-import {
-    Dialog,
-    DialogContent,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger
-} from "@/components/ui/dialog.tsx";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog.tsx";
 import type { Item } from "@/api/generated/openAPIDefinition.schemas.ts";
 import { Button } from "@/components/ui/button.tsx";
 import type { ItemQuantityType } from "@/pages/ItemsPage.tsx";
 import { DialogDescription } from "@radix-ui/react-dialog";
 import { NavLink, useParams } from "react-router-dom";
 import { Separator } from "@/components/ui/separator.tsx";
+import { getGetAllItemsQueryKey, useDeleteItemById } from "@/api/generated/items/items.ts";
+import { toast } from "sonner";
+import { useState } from "react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert.tsx";
+import { Trash2 } from "lucide-react"
+import { motion } from "framer-motion";
+import { useQueryClient } from "@tanstack/react-query";
+import { getGetAllInventoryQueryKey } from "@/api/generated/inventory/inventory.ts";
+
 
 export default function ItemDialogDetailsView( { item, itemQuantity }: Readonly<{
     item: Item,
     itemQuantity?: ItemQuantityType
 }> ) {
 
+    const [ dialogOpen, setDialogOpen ] = useState( false );
+
+    const [ showConfirmDelete, setShowConfirmDelete ] = useState( false );
 
     const { shopId } = useParams();
+
+    const { mutate: deleteItem, isPending } = useDeleteItemById();
+
+    const queryClient = useQueryClient();
 
     const formatDate = ( isoDate: string ) => {
         return new Intl.DateTimeFormat( 'de-DE', {
@@ -31,9 +40,41 @@ export default function ItemDialogDetailsView( { item, itemQuantity }: Readonly<
         } ).format( new Date( isoDate ) );
     };
 
+    const handleDeleteItem = () => {
+        if ( !item.id ) {
+            return;
+        }
+        deleteItem(
+            { id: item.id },
+            {
+                onSuccess: () => {
+                    toast.success( "Item deleted successfully." );
+                    
+                    queryClient.invalidateQueries(
+                        { queryKey: getGetAllItemsQueryKey() }
+                    );
+
+                    queryClient.invalidateQueries(
+                        { queryKey: getGetAllInventoryQueryKey() }
+                    );
+
+                    setDialogOpen( false )
+                },
+                onError: ( error ) => {
+                    toast.error( error.response?.data.message || error.message || "Failed to delete item." );
+                    console.error( "Failed to delete item:", error );
+                }
+            }
+        );
+    }
+
+    const toggleShowConfirmDelete = () => {
+        setShowConfirmDelete( !showConfirmDelete );
+    }
+
 
     return (
-        <Dialog>
+        <Dialog open={ dialogOpen } onOpenChange={ setDialogOpen }>
             <DialogTrigger asChild>
                 <Button variant={ "link" }>
                     Details
@@ -63,9 +104,7 @@ export default function ItemDialogDetailsView( { item, itemQuantity }: Readonly<
                     }
 
                 </div>
-
                 <Separator/>
-
                 <div className={ "grid grid-cols-2 gap-4" }>
                     {
                         Object.entries( itemQuantity || {} ).map( ( [ warehouseName, quantity ] ) => {
@@ -84,12 +123,43 @@ export default function ItemDialogDetailsView( { item, itemQuantity }: Readonly<
                         } )
                     }
                 </div>
-
-                <DialogFooter>
-                    <Button asChild className={ "m-auto mt-4" }>
+                <Separator className={ "my-4" }/>
+                <div className={ "flex justify-between items-center" }>
+                    <Button asChild>
                         <NavLink to={ `/shop/${ shopId }/warehouses` }>See Warehouses</NavLink>
                     </Button>
-                </DialogFooter>
+                    <Button variant={ "destructive" } onClick={ toggleShowConfirmDelete }>
+                        Delete Item
+                    </Button>
+                </div>
+                { showConfirmDelete &&
+                    <motion.div
+                        initial={ { opacity: 0, scale: 0.9 } }
+                        animate={ { opacity: 1, scale: 1 } }
+                        transition={ { duration: 0.2 } }
+                        className={ "mt-4" }
+                    >
+                        <Alert variant={ "destructive" }>
+                            <Trash2/>
+                            <AlertTitle>
+                                Are you sure you want to delete this item?
+                            </AlertTitle>
+                            <AlertDescription>
+                                <p>This actions cannot be undone</p>
+                                <div className={ "flex justify-between mt-4 w-full items-center" }>
+                                    <Button variant={ "destructive" } disabled={ isPending }
+                                            onClick={ handleDeleteItem }>
+                                        Delete Item
+                                    </Button>
+                                    <Button variant={ "outline" } onClick={ toggleShowConfirmDelete }>
+                                        Cancel
+                                    </Button>
+                                </div>
+                            </AlertDescription>
+                        </Alert>
+                    </motion.div>
+                }
+
             </DialogContent>
         </Dialog>
     )
--- frontend/src/components/ItemsPage Comps/ItemsView.tsx
+++ frontend/src/components/ItemsPage Comps/ItemsView.tsx
@@ -1,9 +1,10 @@
 import { type Item } from '@/api/generated/openAPIDefinition.schemas.ts';
 import ItemsSearchBar from "@/components/ItemsPage Comps/ItemsSearchBar.tsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ItemsDefaultView from "@/components/ItemsPage Comps/ItemsDefaultView.tsx";
 import ItemsMobileView from "@/components/ItemsPage Comps/ItemsMobileView.tsx";
 import type { ItemsWithQuantitiesType } from "@/pages/ItemsPage.tsx";
+import CreateItemDialog from "@/components/ItemsPage Comps/CreateItemDialog.tsx";
 
 export type ItemsViewProps = {
     items: ItemViewItem[],
@@ -21,20 +22,34 @@ export default function ItemsView( { items, quantities, quantityLoading }: Reado
 
     const [ filteredItems, setFilteredItems ] = useState<Item[] | []>( items || [] );
 
+    useEffect( () => {
+        if ( !searchQuery ) {
+            // eslint-disable-next-line react-hooks/set-state-in-effect
+            setFilteredItems( items || [] );
+        } else {
+            const filtered = items.filter( ( item ) => {
+                const lowerCaseQuery = searchQuery.toLowerCase();
+                const skuCases = item.sku ? [
+                    item.sku.toLowerCase(),
+                    item.sku.replaceAll( "-", " " ).toLowerCase(),
+                    item.sku.replaceAll( "-", "" ).toLowerCase()
+                ] : [];
+
+                return item.name.toLowerCase().includes( lowerCaseQuery ) ||
+                    skuCases.some( sku => sku.includes( lowerCaseQuery ) );
+            } );
+            setFilteredItems( filtered );
+        }
+    }, [ items, searchQuery ] );
+
     const handleSearchInputChange = ( query: string ) => {
         setSearchQuery( query );
-
-        setFilteredItems( items.filter( ( item ) => {
-            const lowerCaseQuery = query.toLowerCase();
-            const skuCases = item.sku ? [ item.sku.toLowerCase(), item.sku.replaceAll( "-", " " ).toLowerCase(), item.sku.replaceAll( "-", "" ).toLowerCase() ] : [];
-
-            return item.name.toLowerCase().includes( lowerCaseQuery ) || skuCases.some( sku => sku.includes( lowerCaseQuery ) );
-        } ) );
     };
 
     return (
         <div className={ "w-full flex flex-col px-8 mb-8" }>
             <ItemsSearchBar onInputChange={ handleSearchInputChange } inputValue={ searchQuery }/>
+            <CreateItemDialog/>
             {/* Item View <= 768 px (MOBILE)*/ }
             <ItemsMobileView items={ filteredItems } quantities={ quantities } quantityLoading={ quantityLoading }/>
             {/* Item View >= 768 px (TABLET / DESKTOP) */ }
