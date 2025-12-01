051ff6b9b5f3031839bf8eb4ba4b0a96afec6f94
Jan Schwarz <jan-paul@schw-a-rz.de>
2025-12-01 14:58:11 +0100

    Enhance ItemDialogDetailsView with date formatting and additional item details

--- frontend/src/components/ItemsPage Comps/ItemDialogDetailsView.tsx
+++ frontend/src/components/ItemsPage Comps/ItemDialogDetailsView.tsx
@@ -1,14 +1,35 @@
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog.tsx";
+import {
+    Dialog,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger
+} from "@/components/ui/dialog.tsx";
 import type { Item } from "@/api/generated/openAPIDefinition.schemas.ts";
 import { Button } from "@/components/ui/button.tsx";
 import type { ItemQuantityType } from "@/pages/ItemsPage.tsx";
 import { DialogDescription } from "@radix-ui/react-dialog";
-import { NavLink } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
+import { Separator } from "@/components/ui/separator.tsx";
 
 export default function ItemDialogDetailsView( { item, itemQuantity }: Readonly<{
     item: Item,
     itemQuantity?: ItemQuantityType
 }> ) {
+
+
+    const { shopId } = useParams();
+
+    const formatDate = ( isoDate: string ) => {
+        return new Intl.DateTimeFormat( 'de-DE', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit'
+        } ).format( new Date( isoDate ) );
+    };
 
 
     return (
@@ -24,13 +45,27 @@ export default function ItemDialogDetailsView( { item, itemQuantity }: Readonly<
                         { item.name }
                     </DialogTitle>
                     <DialogDescription className={ "text-muted-foreground" }>
-                        Detail view of item
+                        Detail view of Item
                     </DialogDescription>
                 </DialogHeader>
                 <p className={ "text-muted-foreground text-center" }>-- no image available --</p>
-                <p className={ "" }><span
-                    className={ "text-muted-foreground" }>Total #: </span> { itemQuantity?.totalQuantity ?? "n/a" }
-                </p>
+                <Separator/>
+                <div className={ "grid grid-cols-2 gap-2" }>
+                    <p className={ "text-muted-foreground" }>Total #:</p>
+                    <p>{ itemQuantity?.totalQuantity }</p>
+                    <p className={ "text-muted-foreground" }>Created:</p>
+                    <p>{ formatDate( item.createdDate ) }</p>
+                    { item.lastModifiedDate && item.lastModifiedDate !== item.createdDate &&
+                        <>
+                            <p className={ "text-muted-foreground" }>Modified:</p>
+                            <p>{ formatDate( item.lastModifiedDate ) }</p>
+                        </>
+                    }
+
+                </div>
+
+                <Separator/>
+
                 <div className={ "grid grid-cols-2 gap-4" }>
                     {
                         Object.entries( itemQuantity || {} ).map( ( [ warehouseName, quantity ] ) => {
@@ -44,15 +79,17 @@ export default function ItemDialogDetailsView( { item, itemQuantity }: Readonly<
                                     </p>
                                     <span
                                         className={ "text-muted-foreground" }>{ warehouseName }: </span> { quantity }
-                                    <Button asChild variant={ "link" }>
-                                        <NavLink to={ "/" }>Go to warehouse</NavLink>
-
-                                    </Button>
                                 </div>
                             );
                         } )
                     }
                 </div>
+
+                <DialogFooter>
+                    <Button asChild className={ "m-auto mt-4" }>
+                        <NavLink to={ `/shop/${ shopId }/warehouses` }>See Warehouses</NavLink>
+                    </Button>
+                </DialogFooter>
             </DialogContent>
         </Dialog>
     )
